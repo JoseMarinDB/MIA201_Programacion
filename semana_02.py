54d486582e16c3fc4657b3848625b48f7d65b3fd
Jose Marin Udesa <josemarinlic@gmail.com>
2025-04-08 16:10:12 -0300

    add ejercicio 3

--- semana_02.py
+++ semana_02.py
@@ -3,40 +3,30 @@
 lista = [1, 2, 3, 4, 5]
 lista_nombres = ['Bogotá', 'Rosario', 'San Fernando', 'San Miguel']
 
-def invertirLista(l):
-    i=len(l)-1
-    t=[]
-    while i>=0:
-        t.append(l[i])
-        i=i-1
-    return t
 
-def inverirListaRec(l):
+
+def invertir_lista(l):
     if(len(l)==1):
         return l
     else:
-        return invertirLista(l[1:])+ l[0:1]
+        return invertir_lista(l[1:])+ l[0:1]
 
-lista_invertida = inverirListaRec(lista_nombres)
-print(lista_invertida)
 
 
 '''
 Ejercicio 2:
 '''
-def Collatz(n):
+def collatz(n):
     if(n%2==0 and n!=0):
         n=n/2
     else:
         n=n*3+1
-    if(n==1):
+    if(n==1 or n<0):
         return n
     else:
         return Collatz(n)+1
         
 
-resultado = Collatz(0)
-print(resultado)
 
 '''
 Ejercicio 3:
@@ -50,7 +40,6 @@ def contar_definiciones(dic):
     return dic_out
 
 t = contar_definiciones(diccionario)
-print(t)
 
 def cantidad_de_claves_letra(dic, letra):
     cont = 0
@@ -61,7 +50,6 @@ def cantidad_de_claves_letra(dic, letra):
 
 c = cantidad_de_claves_letra(diccionario, "l")
 
-print(c)
 
 
 '''
@@ -82,7 +70,7 @@ quemado = -1
 def propagar(fosforos, fuego=False):
     actual= fosforos[0]
     if(len(fosforos)==1):
-        if (actual== prendido or actual == quemado or (actual ==nuevo and not fuego)):
+        if ( not fuego):
             return [actual]
         else:
             return [prendido]
@@ -92,26 +80,27 @@ def propagar(fosforos, fuego=False):
         elif actual == quemado:
             return [quemado] + (propagar(fosforos[1:],False))
         else:
-            if actual == nuevo:
-                if fuego:
+            if fuego:
+                return [prendido] + (propagar(fosforos[1:],True))
+            else:
+                if(fosforos[1]==prendido):
                     return [prendido] + (propagar(fosforos[1:],True))
-                elif len(fosforos)==1:
-                    return [actual]  
                 else:
-                    if(fosforos[1]==prendido):
-                        return [prendido] + (propagar(fosforos[1:],True))
+                    vuelta = (propagar(fosforos[1:],False))
+                    if vuelta[0]==prendido:
+                        return [prendido] +  vuelta
                     else:
-                        vuelta = (propagar(fosforos[1:],False))
-                        if vuelta[0]==prendido:
-                            return [prendido] +  vuelta
-                        else:
-                            return [actual] +  vuelta
+                        return [actual] +  vuelta
 
 
 
     
 
 propagado = propagar([ 0, 0, 0,-1, 1, 0, 0, 0,-1, 0, 1, 0, 0])
-print(propagado)
-propagado = propagar([ 0, 0, 0, 1, 0, 0])
-print(propagado)
+propagado = propagar([ 0, 0, -1, 1, 0, 0,0,-1,-1,1,1,1,1,0,1,0,-1,0,-1])
+
+
+
+
+
+
